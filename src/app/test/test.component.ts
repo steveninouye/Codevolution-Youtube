e3585ffa7e3ea20e0b4f082c6da0d08a97265a18
Steven Inouye <steveninouye@msn.com>
2018-05-01 15:58:43 -1000

    using ngFor with "first"

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -4,8 +4,8 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-test',
   template: `
 
-<div *ngFor="let color of colors; index as i">
-  <h2>{{i}} {{color}}</h2>
+<div *ngFor="let color of colors; first as f">
+  <h2>{{f}} {{color}}</h2>
 </div>
 
   `,
